f5b03dc3203419623c11e72f3cca8b76a676137f
Abdul-Wasay-Ahmed <48917172+Abdul-Wasay-Ahmed@users.noreply.github.com>
2019-05-05 00:21:59 +0500

    Add files via upload

---
+++ assigment/Pubg/app.js
@@ -0,0 +1,62 @@
+
+function over1() {
+    var x = document.getElementById("myAudio1")
+    x.play();
+}
+function out1() {
+    var x = document.getElementById("myAudio1")
+    x.pause();
+    x.currentTime = 0;
+}
+
+function over2() {
+    var x = document.getElementById("myAudio2")
+    x.play();
+}
+function out2() {
+    var x = document.getElementById("myAudio2")
+    x.pause();
+    x.currentTime = 0;
+}
+
+function over3() {
+    var x = document.getElementById("myAudio3")
+    x.play();
+}
+function out3() {
+    var x = document.getElementById("myAudio3")
+    x.pause();
+    x.currentTime = 0;
+}
+
+function over4() {
+    var x = document.getElementById("myAudio4")
+    x.play();
+}
+function out4() {
+    var x = document.getElementById("myAudio4")
+    x.pause();
+    x.currentTime = 0;
+}
+
+function over5() {
+    var x = document.getElementById("myAudio5")
+    x.play();
+}
+function out5() {
+    var x = document.getElementById("myAudio5")
+    x.pause();
+    x.currentTime = 0;
+}
+function over6() {
+    var x = document.getElementById("myAudio6")
+    x.play();
+}
+function out6() {
+    var x = document.getElementById("myAudio6")
+    x.pause();
+    x.currentTime = 0;
+}
+
+
+
